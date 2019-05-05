d9041f16e5cec77a9dd1361d9b02361e7d782299
Panc4kes <40915904+Panc4kes@users.noreply.github.com>
2019-05-05 16:57:52 +0800

    code re haul

--- ttt.py
+++ ttt.py
@@ -2,27 +2,42 @@ import sys, platform, os
 
 global pos
 
+
 def clear():
     if platform.system() == "Windows":
         os.system("cls")
     else:
         os.system("clear")
 
-__license__ = open("LICENSE","r").read()
+
+__license__ = open("LICENSE", "r").read()
 
 allTurns = 0
 
 pos = {
-    "1":{"1": " ", "2": " ", "3": " ",},
-    "2":{"1": " ", "2": " ", "3": " ",},
-    "3":{"1": " ", "2": " ", "3": " ",}
+    "1": {
+        "1": " ",
+        "2": " ",
+        "3": " ",
+    },
+    "2": {
+        "1": " ",
+        "2": " ",
+        "3": " ",
+    },
+    "3": {
+        "1": " ",
+        "2": " ",
+        "3": " ",
     }
+}
+
 
 def menu():
     clear()
-    print("="*30)
-    print("= "+"Welcome to Tic Tac Toe!".center(26)+" =")
-    print("="*30+"\n")
+    print("=" * 30)
+    print("= " + "Welcome to Tic Tac Toe!".center(26) + " =")
+    print("=" * 30 + "\n")
     print("select an option below")
     print("1 = Player vs Player")
     print("2 = License")
@@ -45,17 +60,20 @@ def menu():
         input()
         menu()
 
+
 def test():
     global pos
     global comb
-    comb = [pos["1"]["1"]+pos["1"]["2"]+pos["1"]["3"],
-       pos["2"]["1"]+pos["2"]["2"]+pos["2"]["3"],
-       pos["3"]["1"]+pos["3"]["2"]+pos["3"]["3"],
-       pos["1"]["1"]+pos["2"]["1"]+pos["3"]["1"],
-       pos["1"]["2"]+pos["2"]["2"]+pos["3"]["2"],
-       pos["1"]["3"]+pos["2"]["3"]+pos["3"]["3"],
-       pos["1"]["1"]+pos["2"]["2"]+pos["3"]["3"],
-       pos["3"]["1"]+pos["2"]["2"]+pos["1"]["3"]]
+    comb = [
+        pos["1"]["1"] + pos["1"]["2"] + pos["1"]["3"],
+        pos["2"]["1"] + pos["2"]["2"] + pos["2"]["3"],
+        pos["3"]["1"] + pos["3"]["2"] + pos["3"]["3"],
+        pos["1"]["1"] + pos["2"]["1"] + pos["3"]["1"],
+        pos["1"]["2"] + pos["2"]["2"] + pos["3"]["2"],
+        pos["1"]["3"] + pos["2"]["3"] + pos["3"]["3"],
+        pos["1"]["1"] + pos["2"]["2"] + pos["3"]["3"],
+        pos["3"]["1"] + pos["2"]["2"] + pos["1"]["3"]
+    ]
 
     if "OOO" in comb:
         print("GAME OVER: PLAYER 1 WON!")
@@ -67,15 +85,17 @@ def test():
         print("GAME OVER: TIE")
         ask()
 
+
 def display():
     print(f'\n[{pos["1"]["1"]}][{pos["2"]["1"]}][{pos["3"]["1"]}]')
     print(f'[{pos["1"]["2"]}][{pos["2"]["2"]}][{pos["3"]["2"]}]')
     print(f'[{pos["1"]["3"]}][{pos["2"]["3"]}][{pos["3"]["3"]}]\n')
 
-def resolve(posi,turn):
+
+def resolve(posi, turn):
     global allTurns
     try:
-        x,y = eval(posi)
+        x, y = eval(posi)
     except:
         print("Invalid coordinates, try again!\n")
         if turn == "X":
@@ -84,7 +104,7 @@ def resolve(posi,turn):
             P1()
     sym = turn
     try:
-        if pos[str(x)][str(y)] in ["X","O"]:
+        if pos[str(x)][str(y)] in ["X", "O"]:
             print("There's already a marker there!")
             display()
             if turn == "X":
@@ -108,17 +128,19 @@ def P1():
     turn = "O"
     print("Give coordinates of placement, in the format of x,y")
     print("Player 1's turn!\n")
-    resolve(input("> "),turn)
+    resolve(input("> "), turn)
     P2()
+
 
 def P2():
     test()
     turn = "X"
     print("Give coordinates of placement, in the format of x,y")
     print("Player 2's turn!\n")
-    resolve(input("> "),turn)
+    resolve(input("> "), turn)
     P1()
-    
+
+
 def ask():
     global pos
     global allTurns
@@ -128,17 +150,30 @@ def ask():
         inp = input("> ")
         if inp.lower() == "n":
             raise SystemExit
-            
+
         elif inp.lower() == "y":
             pos = {
-    "1":{"1": " ", "2": " ", "3": " ",},
-    "2":{"1": " ", "2": " ", "3": " ",},
-    "3":{"1": " ", "2": " ", "3": " ",}
-    }
+                "1": {
+                    "1": " ",
+                    "2": " ",
+                    "3": " ",
+                },
+                "2": {
+                    "1": " ",
+                    "2": " ",
+                    "3": " ",
+                },
+                "3": {
+                    "1": " ",
+                    "2": " ",
+                    "3": " ",
+                }
+            }
             allTurns = 0
             clear()
             P1()
         else:
             print("Undefined keyword")
+
 
 menu()
